bb478b4ab05b86e404d48ccc4d036e643a8ba700
JJ Sermon <thejoshuasermon@gmail.com>
2020-05-29 12:56:11 -0400

    create app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+const api = {
+  key: "dd375dfbfeeb88e76ef19ee90b7d98bd",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return <div className="App">Hello</div>;
